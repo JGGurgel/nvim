48c697d5c7ebee013a87f914c19fe82c3722477c
João Gabriel Campos Gurgel <joaogabriel@rubeus.com.br>
2024-08-22 15:41:06 -0300

    add git integration

--- lua/jonga/packer.lua
+++ lua/jonga/packer.lua
@@ -5,26 +5,26 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     --use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
-     --   vim.cmd('colorscheme rose-pine-moon') 
+    --   vim.cmd('colorscheme rose-pine-moon')
     --end })
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
     use('theprimeagen/harpoon')
 
-    use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-    use({'neovim/nvim-lspconfig'})
-    use({'hrsh7th/nvim-cmp'})
-    use({'hrsh7th/cmp-nvim-lsp'})
+    use({ 'VonHeikemen/lsp-zero.nvim', branch = 'v4.x' })
+    use({ 'neovim/nvim-lspconfig' })
+    use({ 'hrsh7th/nvim-cmp' })
+    use({ 'hrsh7th/cmp-nvim-lsp' })
     use {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim"
     }
     use('tpope/vim-fugitive')
-    use ({'shaunsingh/nord.nvim',config = function() vim.cmd('colorscheme nord') end})
+    use({ 'shaunsingh/nord.nvim', config = function() vim.cmd('colorscheme nord') end })
+    use('ThePrimeagen/vim-be-good')
 end)
-
