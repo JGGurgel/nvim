36ed52f41546f608ce42869d945b34547ffb01e5
João Gabriel Campos Gurgel <joaogabriel@rubeus.com.br>
2025-04-29 10:12:59 -0300

    add git blame bind

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -3,5 +3,6 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 		vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit)
+		vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { noremap = true, silent = true })
 	end,
 }
